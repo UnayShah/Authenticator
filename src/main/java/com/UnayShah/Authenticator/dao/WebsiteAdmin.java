a994faebafd09362e6430d0122c721e83411fcdf
Unay Shah <unayshah@gmail.com>
2021-09-04 20:14:23 +0530

    Added website admin model code

--- src/main/java/com/UnayShah/Authenticator/dao/WebsiteAdmin.java
+++ src/main/java/com/UnayShah/Authenticator/dao/WebsiteAdmin.java
@@ -1,6 +1,7 @@
 package com.UnayShah.Authenticator.dao;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,6 +10,26 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class WebsiteAdmin {
 	@Id
 	String username;
-	String password;
-	List<Website> listWebsites;
+	Set<Website> listWebsites;
+
+	public WebsiteAdmin(String username) {
+		this.username = username;
+		this.listWebsites = new HashSet<>();
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public Set<Website> getListWebsites() {
+		return listWebsites;
+	}
+
+	public Boolean addWebsite(Website website) {
+		return this.listWebsites.add(website);
+	}
+
+	public Boolean removeWebsite(Website website) {
+		return this.listWebsites.remove(website);
+	}
 }
