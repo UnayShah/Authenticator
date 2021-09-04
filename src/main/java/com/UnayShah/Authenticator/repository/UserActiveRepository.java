ca1e31cf3acdb1a2012d346fd764852dd1fcdf16
Unay Shah <unayshah@gmail.com>
2021-09-04 18:01:09 +0530

    Added comments in repository and service.

--- src/main/java/com/UnayShah/Authenticator/repository/UserActiveRepository.java
+++ src/main/java/com/UnayShah/Authenticator/repository/UserActiveRepository.java
@@ -11,6 +11,13 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserActiveRepository extends MongoRepository<UserActive, String> {
 
+    /**
+     * find username and session id combination for active user
+     * 
+     * @param sessionId
+     * @param username
+     * @return
+     */
     @Query(value = "{$and :[{_id: ?0},{username: ?1}] }")
     public Optional<UserActive> findByAllParameters(String sessionId, String username);
 }
--- src/main/java/com/UnayShah/Authenticator/repository/UserInDBRepository.java
+++ src/main/java/com/UnayShah/Authenticator/repository/UserInDBRepository.java
@@ -11,9 +11,25 @@ import com.UnayShah.Authenticator.dao.UserInDB;
 @Repository
 public interface UserInDBRepository extends MongoRepository<UserInDB, String> {
 
+	/**
+	 * find if the username and password combination exist in the database
+	 * 
+	 * @param username
+	 * @param password
+	 * @return
+	 */
 	@Query(value = "{$and: [{_id: ?0}, {password: ?1}]}")
 	public Optional<UserInDB> findByCredentials(String username, String password);
 
+	/**
+	 * find if a username and password exist and the provided website is associated
+	 * with the combination
+	 * 
+	 * @param username
+	 * @param password
+	 * @param websiteId
+	 * @return
+	 */
 	@Query(value = "{$and: [{_id: ?0}, {password: ?1}, {websiteId: {$in: [?2]}}]}")
 	public Optional<UserInDB> findWebsiteOptional(String username, String password, String websiteId);
 }
